0c7796306bec5df507bba2f43dacd8583feb8288
hoangducach <hoangbach0985@gmail.com>
2024-09-20 23:08:02 +0700

    set: default middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,55 +1,55 @@
-import { NextResponse, NextRequest } from 'next/server';
+// import { NextResponse, NextRequest } from 'next/server';
 
-export const config = {
-  matcher: [
-    "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",
-  ],
-};
+// export const config = {
+//   matcher: [
+//     "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",
+//   ],
+// };
 
-const DOCS_URL = process.env.NEXT_PUBLIC_DOCS_URL;
-const ROOT_DOMAIN = process.env.NEXT_PUBLIC_CLIENT_ROOT_DOMAIN;
+// const DOCS_URL = process.env.NEXT_PUBLIC_DOCS_URL;
+// const ROOT_DOMAIN = process.env.NEXT_PUBLIC_CLIENT_ROOT_DOMAIN;
 
-export default async function middleware(req: NextRequest) {
-  const url = req.nextUrl;
+// export default async function middleware(req: NextRequest) {
+//   const url = req.nextUrl;
 
-  // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
-  let hostname = req.headers
-    .get("host")!
-    .replace(".localhost:3000", `.${ROOT_DOMAIN}`)
+//   // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
+//   let hostname = req.headers
+//     .get("host")!
+//     .replace(".localhost:3000", `.${ROOT_DOMAIN}`)
 
-  // special case for Vercel preview deployment URLs
-  if (
-    hostname.includes("---") &&
-    hostname.endsWith(`.${process.env.NEXT_PUBLIC_VERCEL_DEPLOYMENT_SUFFIX}`)
-  ) {
-    hostname = `${hostname.split("---")[0]}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`;
-  }
+//   // special case for Vercel preview deployment URLs
+//   if (
+//     hostname.includes("---") &&
+//     hostname.endsWith(`.${process.env.NEXT_PUBLIC_VERCEL_DEPLOYMENT_SUFFIX}`)
+//   ) {
+//     hostname = `${hostname.split("---")[0]}.${process.env.NEXT_PUBLIC_CLIENT_ROOT_DOMAIN}`;
+//   }
 
-  const searchParams = req.nextUrl.searchParams.toString();
+//   const searchParams = req.nextUrl.searchParams.toString();
   
-  // Get the pathname of the request (e.g. /, /about, /blog/first-post)
-  const path = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ""}`;
+//   // Get the pathname of the request (e.g. /, /about, /blog/first-post)
+//   const path = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ""}`;
 
-  // Rewrite for the landing page
-  if (hostname === process.env.NEXT_PUBLIC_CLIENT_ROOT_DOMAIN) {
-    return NextResponse.rewrite(new URL(path, url));
-  }
+//   // Rewrite for the landing page
+//   if (hostname === process.env.NEXT_PUBLIC_CLIENT_ROOT_DOMAIN) {
+//     return NextResponse.rewrite(new URL(path, url));
+//   }
     
 
-  // rewrites for app pages
-  if (hostname == `app.${process.env.NEXT_PUBLIC_CLIENT_ROOT_DOMAIN}`) {
-    if (path === "/") {
-      return NextResponse.redirect(new URL("/dashboard", url));
-    }
-    return NextResponse.rewrite(new URL(path, url));
-  }
+//   // rewrites for app pages
+//   if (hostname == `app.${process.env.NEXT_PUBLIC_CLIENT_ROOT_DOMAIN}`) {
+//     if (path === "/") {
+//       return NextResponse.redirect(new URL("/dashboard", url));
+//     }
+//     return NextResponse.rewrite(new URL(path, url));
+//   }
 
-  // special case for doc pages
-  if (hostname === `docs.${process.env.NEXT_PUBLIC_CLIENT_ROOT_DOMAIN}`) {
-    return NextResponse.redirect(
-      "https://x404pump.gitbook.io/x404pump-docs",
-    );
-  }
+//   // special case for doc pages
+//   if (hostname === `docs.${process.env.NEXT_PUBLIC_CLIENT_ROOT_DOMAIN}`) {
+//     return NextResponse.redirect(
+//       "https://x404pump.gitbook.io/x404pump-docs",
+//     );
+//   }
 
-  return new Response("Not Found", { status: 404 });
-}
+//   return new Response("Not Found", { status: 404 });
+// }
